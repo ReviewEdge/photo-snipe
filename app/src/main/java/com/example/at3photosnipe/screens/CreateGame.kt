7289b694959221e74453ee00dfd931f8e1e2353e
dytryk <astonmartinone111111@gmail.com>
2023-11-14 16:16:08 -0500

    push1

--- app/src/main/java/com/example/at3photosnipe/screens/CreateGame.kt
+++ app/src/main/java/com/example/at3photosnipe/screens/CreateGame.kt
@@ -1,12 +1,16 @@
 package com.example.at3photosnipe.screens
 
 import android.icu.text.CaseMap
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -14,13 +18,28 @@ import androidx.compose.ui.unit.sp
 
 @Composable
 fun CreateGame(StartGame: () -> Unit){
-    Column(modifier = Modifier.fillMaxSize()){
-        Text(text = "Create Game Screen")
-    }
-    Button(onClick = {StartGame()},
-        modifier = Modifier.padding(12.dp)
-    ) {
-        Text(text = "Create Game")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ){
+            Text(text = "Create Game Screen", modifier = Modifier.padding(16.dp), fontSize = 32.sp)
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = {StartGame()},
+                modifier = Modifier.padding(12.dp)
+            ) {
+                Text(text = "Create Game")
+            }
+        }
     }
 }
 
--- app/src/main/java/com/example/at3photosnipe/screens/MainScreen.kt
+++ app/src/main/java/com/example/at3photosnipe/screens/MainScreen.kt
@@ -1,24 +1,44 @@
 package com.example.at3photosnipe.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun MainGame(Map: () -> Unit){
-    Column(modifier = Modifier.fillMaxSize()){
-        Text(text = "Main Game Screen")
-    }
-    Button(onClick = {Map()},
-        modifier = Modifier.padding(12.dp)
-    ) {
-        Text(text = "View Map")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ){
+            Text(text = "Main Game Screen", modifier = Modifier.padding(16.dp), fontSize = 32.sp)
+        }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Button(onClick = {Map()},
+                modifier = Modifier.padding(12.dp)
+            ) {
+                Text(text = "View Map")
+            }
+        }
     }
 }
 
