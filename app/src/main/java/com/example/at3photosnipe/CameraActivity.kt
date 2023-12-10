37b0d229f147478b98a8ee480a1134878792e165
dytryk <astonmartinone111111@gmail.com>
2023-12-10 16:31:01 -0500

    cameraWorking

--- app/src/main/java/com/example/at3photosnipe/CameraActivity.kt
+++ app/src/main/java/com/example/at3photosnipe/CameraActivity.kt
@@ -2,6 +2,7 @@ package com.example.at3photosnipe
 
 import CameraView
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
@@ -10,10 +11,19 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType.Companion.Text
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import coil.compose.rememberImagePainter
@@ -40,11 +50,18 @@ class CameraActivity: ComponentActivity() {
         shouldShowPhoto.value = true
     }
 
+//    private fun getOutputDirectory(): File {
+//        val mediaDir = R.drawable
+////        val mediaDir = externalMediaDirs.firstOrNull()?.let {
+////            File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
+////        }
+//        return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
+//    }
+
     private fun getOutputDirectory(): File {
         val mediaDir = externalMediaDirs.firstOrNull()?.let {
             File(it, resources.getString(R.string.app_name)).apply { mkdirs() }
         }
-
         return if (mediaDir != null && mediaDir.exists()) mediaDir else filesDir
     }
 
@@ -92,11 +109,34 @@ class CameraActivity: ComponentActivity() {
                 )
             }
             if (shouldShowPhoto.value) {
-                Image(
-                    painter = rememberImagePainter(photoUri),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize()
-                )
+                Column(){
+                    Image(
+                        painter = rememberImagePainter(photoUri),
+                        contentDescription = null,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+
+                    val lContext = LocalContext.current
+
+                    Row(){
+                        Button(
+                            onClick = { finish()
+                                lContext.startActivity(Intent(lContext, CameraActivity::class.java))
+                            },
+                            modifier = Modifier.padding(12.dp)
+                        ) {
+                            Text(text = "Cancel")
+                        }
+                        Button(
+//                            onClick = { lContext.startActivity(Intent(lContext, AfterSnipeActivity::class.java)) },
+                            onClick = { finish() },
+                            modifier = Modifier.padding(12.dp)
+                        ) {
+                            Text(text = "Save Snipe")
+                            isCameraScreen = false
+                        }
+                    }
+                }
             }
         }
         requestCameraPermission()
